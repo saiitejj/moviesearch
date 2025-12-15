6a3c490e7dc2d43abf7d76c1824c60feb71ec42e
Sai Tej <ngvst2004@gmail.com>
2025-12-16 03:29:12 +0530

    Backend Deployed

--- server/index.js
+++ server/index.js
@@ -1,11 +1,11 @@
 import express from "express"
-// import cors from "cors"
+import cors from "cors"
 import moviesRoutes from "./Routes/moviesRoute.js";
 
 const app=express()
 const port=3000;
 
-// app.use(cors())
+app.use(cors())
 app.use(express.json())
 
 app.get('/',(req,res)=>{
