b4d405229d94dac3128b5e9109ab92c887c98bf1
Sai Tej <ngvst2004@gmail.com>
2025-12-16 22:31:30 +0530

    feat:Added CSS to Main Page

--- client/src/pages/MainPage.jsx
+++ client/src/pages/MainPage.jsx
@@ -5,15 +5,31 @@ const MainPage=()=>{
     const navigate=useNavigate()
     return (
     
-        <div>
+        <div className="min-h-screen bg-[url('/movie.jpg')] bg-cover bg-center bg-no-repeat flex border items-center justify-center ">
+            <div className="w-96 border p-8 text-center space-y-6 bg-white/30 backdrop-blur-md shadow-lg rounded-lg border-transparent ">
+                <form className="flex flex-col space-y-4 p-2">
 
-            <h1>Main Page</h1>
-            <button onClick={()=>navigate('/moviesFree')}>
-                Free movies
-            </button>
-            <button onClick={()=>navigate('/moviesPaid')}>
-                Paid movies
-            </button>
+                    <h1 className="text-xl font-bold text-gray-900">GET MOVIE URL SUGGESTIONS</h1>
+                    <div>
+
+
+                    <button 
+                    type="button"
+                    onClick={()=>navigate('/moviesFree')}
+                        className="border p-2 cursor-pointer rounded-sm mr-2 ring-0 hover:ring-[#213448] hover:ring-2 focus:outline-none border-transparent shadow-md "
+                        >
+                        Free movies 
+                    </button >
+                    <button 
+                        type="button"
+                    onClick={()=>navigate('/moviesPaid')}
+                        className="border p-2 cursor-pointer rounded-sm mr-2 hover:ring-[#213448] hover:ring-2 focus:outline-dotted border-transparent shadow-md"
+                        >
+                        Paid movies
+                    </button>
+                    </div>
+                </form>
+            </div>
         </div>
 
     )
